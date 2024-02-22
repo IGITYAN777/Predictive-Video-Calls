8b5340843d8594e1cb63f0b194140101125c8fef
IGITYAN777 <122623910+IGITYAN777@users.noreply.github.com>
2024-02-22 18:08:29 +0400

    Add files via upload

---
+++ Task6/script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn1").addEventListener("click", function(){
+	document.getElementById("homepage").style.display = "block";
+});
+
+document.getElementById("btn2").addEventListener("click", function(){
+	document.getElementById("homepage").style.display = "none";
+});
