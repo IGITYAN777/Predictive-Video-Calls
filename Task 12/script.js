1b673bf24e229012bc61bdeb4439a7630b05f2cb
IGITYAN777 <122623910+IGITYAN777@users.noreply.github.com>
2024-02-23 20:02:54 +0400

    Add files via upload

---
+++ Task 12/script.js
@@ -0,0 +1,29 @@
+h1hover.onmouseover = function(event){
+	let target = event.target;
+	target.style.color = 'orange';
+}
+
+h1hover.onmouseout = function(event){
+	let target = event.target;
+	target.style.color = '';
+}
+
+btn1.onmouseover = function(event){
+	let target = event.target;
+	target.style.backgroundColor = 'red';
+}
+
+btn1.onmouseout = function(event){
+	let target = event.target;
+	target.style.backgroundColor = '';
+}
+
+btn2.onmouseover = function(event){
+	let target = event.target;
+	target.style.backgroundColor = 'green';
+}
+
+btn2.onmouseout = function(event){
+	let target = event.target;
+	target.style.backgroundColor = '';
+}
